f9a523a9125066c75d370c5a7d110f53cc6a5a4d
Asmamaw-Abeba <asmeabeba12@gmail.com>
2024-10-27 02:57:19 -0700

    add images and slide stop button

--- images.js
+++ images.js
@@ -1,19 +1,37 @@
-const imageContainer = [
-         'images/image-1.jpg',
-         'images/image-2.png',
-         'images/image-3.jpg',
-         'images/image-4.png',
-         'images/image-5.jpg'
-         ];
+import { gallary } from "./gallary.js";
+console.log(gallary);
 
-      const image = document.querySelector('.images');
-      let  index = 0;
-      
-      function changeImage() {
-        image.src = imageContainer[index];
-        index = (index + 1) % imageContainer.length;
-      }
-      changeImage();
+console.log(gallary.length);
+
+// const imageContainer = [
+//   'images/image-1.jpg',
+//   'images/image-2.png',
+//   'images/image-3.jpg',
+//   'images/image-4.png',
+//   'images/image-5.jpg'
+//   ];
+
+const image = document.querySelector('.images');
+let  index = 0;
+
+function changeImage() {
+  image.src = gallary[index];
+  index = (index + 1) % gallary.length;
+
+  const slideCounter = document.querySelector('.slide');
+  slideCounter.innerHTML = 'slide: ' + index;
+}
+changeImage();
+
+let intervals = setInterval(changeImage, 3000);
+
+const controlerBtn = document.querySelector('.stop-button');
+  controlerBtn.addEventListener('click', () => {
+    if(controlerBtn.innerText === 'stop') {
+      clearInterval(intervals);
+    }
     
+    
+    controlerBtn.classList.add(resume);
+  });
 
-      setInterval(changeImage, 3000);
